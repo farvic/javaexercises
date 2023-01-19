f57a0e857d6c8d44a667f777cb733e497f08c46e
Victor <vicfaraujo@gmail.com>
2023-01-19 15:16:31 -0300

    [Learning Spring] Chap3. IoC

--- learning-spring/src/main/java/com/victoraraujo/lil/learningspring/data/ReservationRepository.java
+++ learning-spring/src/main/java/com/victoraraujo/lil/learningspring/data/ReservationRepository.java
@@ -3,9 +3,7 @@ package com.victoraraujo.lil.learningspring.data;
 import java.sql.Date;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     Iterable<Reservation> findReservationByReservationDate(Date date);
 }
--- learning-spring/src/main/java/com/victoraraujo/lil/learningspring/data/RoomRepository.java
+++ learning-spring/src/main/java/com/victoraraujo/lil/learningspring/data/RoomRepository.java
@@ -1,8 +1,6 @@
 package com.victoraraujo.lil.learningspring.data;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RoomRepository extends CrudRepository<Room, Long> {
 }

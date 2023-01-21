c521d81b735e2be8642b0ffbd790a38671922080
Victor <vicfaraujo@gmail.com>
2023-01-21 17:31:39 -0300

    [First Microservice] Chap4. RESTful API with Spring MVC

---
+++ explorecali/src/main/java/com/example/ec/repo/TourRepository.java
@@ -0,0 +1,50 @@
+package com.example.ec.repo;
+
+import com.example.ec.domain.Tour;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+/**
+ * Tour Repository Interface
+ *
+ * Created by Mary Ellen Bowman
+ */
+public interface TourRepository extends JpaRepository<Tour, Integer> {
+    /**
+     * Find Tours associated with the Tour Package.
+     *
+     * @param code tour package code
+     * @return List of found tours.
+     */
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> List<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+}

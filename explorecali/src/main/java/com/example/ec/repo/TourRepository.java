9d6c03cd23c52f5a6606197f60d8beaae15e52a2
Victor <vicfaraujo@gmail.com>
2023-01-22 16:20:42 -0300

    [Microservice course] Mongodb integration error

--- explorecali/src/main/java/com/example/ec/repo/TourRepository.java
+++ explorecali/src/main/java/com/example/ec/repo/TourRepository.java
@@ -1,10 +1,15 @@
 package com.example.ec.repo;
 
 import com.example.ec.domain.Tour;
+
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
+// import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -13,7 +18,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
  *
  * Created by Mary Ellen Bowman
  */
-public interface TourRepository extends PagingAndSortingRepository<Tour, String> {
+public interface TourRepository extends MongoRepository<Tour, String> {
     /**
      * Find Tours associated with the Tour Package.
      *
@@ -37,7 +42,7 @@ public interface TourRepository extends PagingAndSortingRepository<Tour, String>
 
     @Override
     @RestResource(exported = false)
-    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+    <S extends Tour> List<S> saveAll(Iterable<S> iterable);
 
     @Override
     @RestResource(exported = false)

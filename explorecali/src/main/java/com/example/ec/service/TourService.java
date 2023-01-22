7ebf79a78bca6db4abac0a7c9f9e3ea6f2808d08
Victor <vicfaraujo@gmail.com>
2023-01-22 02:05:39 -0300

    [First Microservice] Chap5 with BeanOverrideException

--- explorecali/src/main/java/com/example/ec/service/TourService.java
+++ explorecali/src/main/java/com/example/ec/service/TourService.java
@@ -1,13 +1,13 @@
 package com.example.ec.service;
 
-import com.example.ec.domain.Difficulty;
-import com.example.ec.domain.Region;
 import com.example.ec.domain.Tour;
 import com.example.ec.domain.TourPackage;
 import com.example.ec.repo.TourPackageRepository;
 import com.example.ec.repo.TourRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 /**
  * Tour Service
@@ -19,35 +19,23 @@ public class TourService {
     private TourRepository tourRepository;
     private TourPackageRepository tourPackageRepository;
 
-    @Autowired
     public TourService(TourRepository tourRepository, TourPackageRepository tourPackageRepository) {
         this.tourRepository = tourRepository;
         this.tourPackageRepository = tourPackageRepository;
     }
 
     /**
-     * Create a new Tour Object and persist it to the Database.
+     * Create a new Tour Object and persist it to the Database
      *
-     * @param title           title
-     * @param description     description
-     * @param blurb           blurb
-     * @param price           price
-     * @param duration        duration
-     * @param bullets         bullets
-     * @param keywords        keywords
-     * @param tourPackageName tour package name
-     * @param difficulty      difficulty
-     * @param region          region
-     * @return Tour Entity
+     * @param title           Title of the tour
+     * @param tourPackageName tour Package of the tour
+     * @param details         Extra details about the tour
+     * @return Tour
      */
-    public Tour createTour(String title, String description, String blurb, Integer price,
-            String duration, String bullets,
-            String keywords, String tourPackageName, Difficulty difficulty, Region region) {
+    public Tour createTour(String title, String tourPackageName, Map<String, String> details) {
         TourPackage tourPackage = tourPackageRepository.findByName(tourPackageName)
                 .orElseThrow(() -> new RuntimeException("Tour package does not exist: " + tourPackageName));
-
-        return tourRepository.save(new Tour(title, description, blurb, price, duration,
-                bullets, keywords, tourPackage, difficulty, region));
+        return tourRepository.save(new Tour(title, tourPackage, details));
     }
 
     /**

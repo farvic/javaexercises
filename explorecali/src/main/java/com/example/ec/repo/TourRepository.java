46775516b3753b189688c64473435d24807fdec3
Victor <vicfaraujo@gmail.com>
2023-01-23 10:47:12 -0300

    [Ext, Sec and Dockerizing] before creating branch

--- explorecali/src/main/java/com/example/ec/repo/TourRepository.java
+++ explorecali/src/main/java/com/example/ec/repo/TourRepository.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -15,7 +16,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
  *
  * Created by Mary Ellen Bowman
  */
-public interface TourRepository extends JpaRepository<Tour, Integer> {
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer>, CrudRepository<Tour, Integer> {
     /**
      * Find Tours associated with the Tour Package.
      *

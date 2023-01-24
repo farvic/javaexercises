e7a7ecc39c42823dd30c19c1b095e13d427afa7f
Victor <vicfaraujo@gmail.com>
2023-01-24 14:51:46 -0300

    [Ext, Sec and Dockerizing] Chap2. Adding a runtime Logger

--- explorecali/src/main/java/com/example/ec/service/TourRatingService.java
+++ explorecali/src/main/java/com/example/ec/service/TourRatingService.java
@@ -4,16 +4,14 @@ import com.example.ec.domain.Tour;
 import com.example.ec.domain.TourRating;
 import com.example.ec.repo.TourRatingRepository;
 import com.example.ec.repo.TourRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.OptionalDouble;
+import java.util.*;
 
 /**
  * Tour Rating Service
@@ -21,8 +19,8 @@ import java.util.OptionalDouble;
  * Created by Mary Ellen Bowman.
  */
 @Service
-@Transactional
 public class TourRatingService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TourRatingService.class);
     private TourRatingRepository tourRatingRepository;
     private TourRepository tourRepository;
 
@@ -48,6 +46,7 @@ public class TourRatingService {
      * @throws NoSuchElementException if no Tour found.
      */
     public void createNew(int tourId, Integer customerId, Integer score, String comment) throws NoSuchElementException {
+        LOGGER.info("Create Rating for tour {} of customers {}", tourId, customerId);
         tourRatingRepository.save(new TourRating(verifyTour(tourId), customerId,
                 score, comment));
     }
@@ -68,6 +67,7 @@ public class TourRatingService {
      * @return List of TourRatings
      */
     public List<TourRating> lookupAll() {
+        LOGGER.info("Lookup all Ratings");
         return tourRatingRepository.findAll();
     }
 
@@ -80,6 +80,7 @@ public class TourRatingService {
      * @throws NoSuchElementException if no Tour found.
      */
     public Page<TourRating> lookupRatings(int tourId, Pageable pageable) throws NoSuchElementException {
+        LOGGER.info("Lookup Rating for tour {}", tourId);
         return tourRatingRepository.findByTourId(verifyTour(tourId).getId(), pageable);
     }
 
@@ -94,6 +95,7 @@ public class TourRatingService {
      */
     public TourRating update(int tourId, Integer customerId, Integer score, String comment)
             throws NoSuchElementException {
+        LOGGER.info("Update all of Rating for tour {} of customers {}", tourId, customerId);
         TourRating rating = verifyTourRating(tourId, customerId);
         rating.setScore(score);
         rating.setComment(comment);
@@ -112,6 +114,7 @@ public class TourRatingService {
      */
     public TourRating updateSome(int tourId, Integer customerId, Integer score, String comment)
             throws NoSuchElementException {
+        LOGGER.info("Update some of Rating for tour {} of customers {}", tourId, customerId);
         TourRating rating = verifyTourRating(tourId, customerId);
         if (score != null) {
             rating.setScore(score);
@@ -130,6 +133,7 @@ public class TourRatingService {
      * @throws NoSuchElementException if no Tour found.
      */
     public void delete(int tourId, Integer customerId) throws NoSuchElementException {
+        LOGGER.info("Delete Rating for tour {} and customer {}", tourId, customerId);
         TourRating rating = verifyTourRating(tourId, customerId);
         tourRatingRepository.delete(rating);
     }
@@ -142,6 +146,7 @@ public class TourRatingService {
      * @throws NoSuchElementException
      */
     public Double getAverageScore(int tourId) throws NoSuchElementException {
+        LOGGER.info("Get average score of tour {} by customers {}", tourId);
         List<TourRating> ratings = tourRatingRepository.findByTourId(verifyTour(tourId).getId());
         OptionalDouble average = ratings.stream().mapToInt((rating) -> rating.getScore()).average();
         return average.isPresent() ? average.getAsDouble() : null;
@@ -155,11 +160,12 @@ public class TourRatingService {
      * @param customers
      */
     public void rateMany(int tourId, int score, Integer[] customers) {
-        tourRepository.findById(tourId).ifPresent(tour -> {
-            for (Integer c : customers) {
-                tourRatingRepository.save(new TourRating(tour, c, score));
-            }
-        });
+        LOGGER.info("Rate tour {} by customers {}", tourId, Arrays.asList(customers).toString());
+        Tour tour = tourRepository.findById(tourId).orElseThrow(() -> new NoSuchElementException());
+        for (Integer c : customers) {
+            LOGGER.debug("Attempt to create Tour Rating for customer {}", c);
+            tourRatingRepository.save(new TourRating(tour, c, score));
+        }
     }
 
     /**
--- explorecali/src/main/java/com/example/ec/web/TourRatingController.java
+++ explorecali/src/main/java/com/example/ec/web/TourRatingController.java
@@ -2,10 +2,16 @@ package com.example.ec.web;
 
 import com.example.ec.domain.TourRating;
 import com.example.ec.service.TourRatingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,11 +29,15 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping(path = "/tours/{tourId}/ratings")
 public class TourRatingController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TourRatingController.class);
     private TourRatingService tourRatingService;
+    private RatingAssembler assembler;
 
     @Autowired
-    public TourRatingController(TourRatingService tourRatingService) {
+    public TourRatingController(TourRatingService tourRatingService,
+            RatingAssembler assembler) {
         this.tourRatingService = tourRatingService;
+        this.assembler = assembler;
     }
 
     protected TourRatingController() {
@@ -44,6 +54,7 @@ public class TourRatingController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createTourRating(@PathVariable(value = "tourId") int tourId,
             @RequestBody @Validated RatingDto ratingDto) {
+        LOGGER.info("POST /tours/{}/ratings", tourId);
         tourRatingService.createNew(tourId, ratingDto.getCustomerId(), ratingDto.getScore(), ratingDto.getComment());
     }
 
@@ -59,22 +70,36 @@ public class TourRatingController {
     public void createManyTourRatings(@PathVariable(value = "tourId") int tourId,
             @PathVariable(value = "score") int score,
             @RequestParam("customers") Integer customers[]) {
+        LOGGER.info("POST /tours/{}/ratings/{}", tourId, score);
         tourRatingService.rateMany(tourId, score, customers);
     }
+
+    // @GetMapping
+    // public Page<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId")
+    // int tourId, Pageable pageable) {
+    // Page<TourRating> tourRatingPage = tourRatingService.lookupRatings(tourId,
+    // pageable);
+    // List<RatingDto> ratingDtoList = tourRatingPage.getContent()
+    // .stream().map(tourRating -> toDto(tourRating)).collect(Collectors.toList());
+    // return new PageImpl<RatingDto>(ratingDtoList, pageable,
+    // tourRatingPage.getTotalPages());
+    // }
 
     /**
      * Lookup a the Ratings for a tour.
      *
      * @param tourId
      * @param pageable
-     * @return
+     * @param pagedAssembler
+     * @return HATEOAS enabled page of ratings.
      */
     @GetMapping
-    public Page<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId, Pageable pageable) {
-        Page<TourRating> tourRatingPage = tourRatingService.lookupRatings(tourId, pageable);
-        List<RatingDto> ratingDtoList = tourRatingPage.getContent()
-                .stream().map(tourRating -> toDto(tourRating)).collect(Collectors.toList());
-        return new PageImpl<RatingDto>(ratingDtoList, pageable, tourRatingPage.getTotalPages());
+    public PagedModel<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId, Pageable pageable,
+            PagedResourcesAssembler<TourRating> pagedAssembler) {
+        LOGGER.info("GET /tours/{}/ratings", tourId);
+        Page<TourRating> tourRatingPage = tourRatingService.lookupRatings(tourId,
+                pageable);
+        return pagedAssembler.toModel(tourRatingPage, assembler);
     }
 
     /**
@@ -85,6 +110,7 @@ public class TourRatingController {
      */
     @GetMapping("/average")
     public AbstractMap.SimpleEntry<String, Double> getAverage(@PathVariable(value = "tourId") int tourId) {
+        LOGGER.info("GET /tours/{}/ratings/average", tourId);
         return new AbstractMap.SimpleEntry<String, Double>("average", tourRatingService.getAverageScore(tourId));
     }
 
@@ -98,6 +124,7 @@ public class TourRatingController {
     @PutMapping
     public RatingDto updateWithPut(@PathVariable(value = "tourId") int tourId,
             @RequestBody @Validated RatingDto ratingDto) {
+        LOGGER.info("PUT /tours/{}/ratings", tourId);
         return toDto(tourRatingService.update(tourId, ratingDto.getCustomerId(),
                 ratingDto.getScore(), ratingDto.getComment()));
     }
@@ -112,6 +139,7 @@ public class TourRatingController {
     @PatchMapping
     public RatingDto updateWithPatch(@PathVariable(value = "tourId") int tourId,
             @RequestBody @Validated RatingDto ratingDto) {
+        LOGGER.info("PATCH /tours/{}/ratings", tourId);
         return toDto(tourRatingService.updateSome(tourId, ratingDto.getCustomerId(),
                 ratingDto.getScore(), ratingDto.getComment()));
     }
@@ -124,6 +152,7 @@ public class TourRatingController {
      */
     @DeleteMapping("/{customerId}")
     public void delete(@PathVariable(value = "tourId") int tourId, @PathVariable(value = "customerId") int customerId) {
+        LOGGER.info("DELETE /tours/{}/ratings/{}", tourId, customerId);
         tourRatingService.delete(tourId, customerId);
     }
 
@@ -134,7 +163,7 @@ public class TourRatingController {
      * @return RatingDto
      */
     private RatingDto toDto(TourRating tourRating) {
-        return new RatingDto(tourRating.getScore(), tourRating.getComment(), tourRating.getCustomerId());
+        return assembler.toModel(tourRating);
     }
 
     /**
@@ -145,7 +174,8 @@ public class TourRatingController {
      */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
-    public String return400(NoSuchElementException ex) {
+    public String return404(NoSuchElementException ex) {
+        LOGGER.error("Unable to complete transaction", ex);
         return ex.getMessage();
 
     }

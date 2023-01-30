de4c66a2e7c2f32590eb5523e5d2366ac197a27b
Victor <vicfaraujo@gmail.com>
2023-01-30 02:44:21 -0300

    [Spring&React] CRM with submit done

---
+++ crmspringreact/src/test/java/com/farvic/crmspringreact/CrmspringreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farvic.crmspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmspringreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
